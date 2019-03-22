087f40ee51aa38efe7708fad96aabd77b22fe370
Sergi Chalauri <sergi.chalauri@gmail.com>
2019-03-23 01:28:37 +0400

    Logging users in in automated bot

--- automated_bot.py
+++ automated_bot.py
@@ -1,19 +1,83 @@
 import argparse
 import configparser
 import requests
+import random
+import string
+
+from faker import Faker
 
 
 URL_BASE = "http://localhost:8000/api/"
 
 
-def run_automated_bot(number_of_users, max_posts_per_user, max_likes_per_user):
-    # resp = requests.post(URL_BASE + 'auth/register/', json={
-    #     'username': 'uname',
-    #     'email': 'uname@domain.com',
-    #     'password': 'uname1234'
-    # })
-    # print(resp.json())
-    pass
+class SimpleApiClient:
+    def __init__(self, url_base=URL_BASE):
+        self.url_base = url_base
+
+    def register_user(self, user_registration_data):
+        response = requests.post(
+            URL_BASE + "auth/register/", json=user_registration_data
+        )
+        if response.status_code == 201:
+            return response.json()
+        raise Exception("Registration failed for some reason")
+
+    def log_in(self, username, password):
+        response = requests.post(
+            URL_BASE + "auth/token/", json={"username": username, "password": password}
+        )
+        if response.status_code == 200:
+            return response.json()
+        raise Exception("Login failed for some reason")
+
+
+def _random_alphanumeric_string(len=10):
+    characters = string.ascii_letters + string.digits
+    return "".join(random.choice(characters) for i in range(len))
+
+
+def _random_numeric_string(len=3):
+    characters = string.digits
+    return "".join(random.choice(characters) for _ in range(len))
+
+
+def _generate_user_data(number_of_users, faker):
+    fake_username = lambda: faker.name().split(" ")[0].lower() + _random_numeric_string(
+        len=2
+    )
+    fake_usernames = [fake_username() for _ in range(number_of_users)]
+    return [
+        {
+            "username": fake_username,
+            "password": _random_alphanumeric_string(),
+            "email": string.Template("$username@starnavi.io").substitute(
+                username=fake_username
+            ),
+        }
+        for fake_username in fake_usernames
+    ]
+
+
+def _log_users_in(registered_users, simple_api_client):
+    for registered_user in registered_users:
+        token_pair = simple_api_client.log_in(
+            registered_user["user_registration_data"]["username"],
+            registered_user["user_registration_data"]["password"],
+        )
+        registered_user["token_pair"] = token_pair
+
+
+def run_automated_bot(faker, number_of_users, max_posts_per_user, max_likes_per_user):
+    user_registration_data_list = _generate_user_data(number_of_users, faker)
+    simple_api_client = SimpleApiClient()
+    registered_users = [
+        {
+            "user_id": simple_api_client.register_user(user_registration_data)["id"],
+            "user_registration_data": user_registration_data,
+        }
+        for user_registration_data in user_registration_data_list
+    ]
+    _log_users_in(registered_users, simple_api_client)
 
 
 def main():
@@ -27,7 +91,7 @@ def main():
     max_posts_per_user = int(config.get("Basic Params", "max_posts_per_user"))
     max_likes_per_user = int(config.get("Basic Params", "max_likes_per_user"))
 
-    run_automated_bot(number_of_users, max_posts_per_user, max_likes_per_user)
+    run_automated_bot(Faker(), number_of_users, max_posts_per_user, max_likes_per_user)
 
 
 if __name__ == "__main__":
